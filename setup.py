7d1301d35cc7878abfe01472d9cb5151207bbe93
Cedrik Neumann <m1racoli@users.noreply.github.com>
2016-05-04 11:14:52 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='airconditioner',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['airconditioner', 'constructors'],
     url='https://github.com/wooga/bit.airconditioner',
     license='',
